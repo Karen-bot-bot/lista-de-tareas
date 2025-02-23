3136246f70ec8c4c2aba1d75a3e8d62640fb1241
Karen Luna <karenlunafeldheim@gmail.com>
2025-02-23 14:17:30 -0300

    Agregar estructura básica y enlace con JS

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// fecha
+
+const dateNumber = document.querySelector("#dateNumber");
+const dateMonth = document.querySelector("#dateMonth");
+const dateYear = document.querySelector("#dateYear");
+const dateText = document.querySelector("#dateText");
+
+const tasksContainer = document.querySelector("#tasksContainer");
